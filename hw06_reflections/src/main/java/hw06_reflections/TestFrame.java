107166ae6e9af515158765773dcbb4c6aca0e071
Zoufar <zuf_13@mail.ru>
2021-01-16 14:23:33 +0300

    hw_06 amendment

--- hw06_reflections/src/main/java/hw06_reflections/TestFrame.java
+++ hw06_reflections/src/main/java/hw06_reflections/TestFrame.java
@@ -8,11 +8,14 @@ import java.util.stream.Collectors;
 import java.util.ArrayList;
 
 public class TestFrame {
-    Class<?>clazz;
-    TestFrame(Class<?>cl){this.clazz=cl;}
+    Class<?> clazz;
 
-    public void runTests()  throws ReflectiveOperationException {
-        System.out.println("testing class:          "+ clazz);
+    TestFrame(Class<?> cl) {
+        this.clazz = cl;
+    }
+
+    public void runTests() throws ReflectiveOperationException {
+        System.out.println("testing class:          " + clazz);
 
         Method[] methodsAll = clazz.getDeclaredMethods();
         System.out.println("\n--- all methods:");
@@ -23,36 +26,40 @@ public class TestFrame {
         List<Method> methodsAfter = new ArrayList<>();
         List<Method> methodsTest = new ArrayList<>();
         methodsBefore = Arrays.stream(methodsAll).
-                filter(method -> method.getDeclaredAnnotation(Before.class)!=null).
+                filter(method -> method.getDeclaredAnnotation(Before.class) != null).
                 collect(Collectors.toList());
         methodsAfter = Arrays.stream(methodsAll).
-                filter(method -> method.getDeclaredAnnotation(After.class)!=null).
+                filter(method -> method.getDeclaredAnnotation(After.class) != null).
                 collect(Collectors.toList());
         methodsTest = Arrays.stream(methodsAll).
-                filter(method -> method.getDeclaredAnnotation(Test.class)!=null).
+                filter(method -> method.getDeclaredAnnotation(Test.class) != null).
                 collect(Collectors.toList());
 
         var constr = clazz.getConstructor();
         int nOK = 0, nExcptn = 0;
 
-        for(var methodTest:methodsTest) {
-            var obj = constr.newInstance();
-            String str="method   " + methodTest.getName() + " called";
+        for (var methodTest : methodsTest) {
+            var objForTest = constr.newInstance();
+            String str = "method   " + methodTest.getName() + " called";
 
-            for(var methodBefore:methodsBefore){callMethod(obj, methodBefore.getName());}
-            try {
-                callMethod(obj, methodTest.getName());
-                nOK += 1;
+            for (var methodBefore : methodsBefore) {
+                var objForBefore = constr.newInstance();
+                callMethod(objForBefore, methodBefore.getName());
             }
-            catch (RuntimeException e) {
+            try {
+                callMethod(objForTest, methodTest.getName());
+                nOK += 1;
+            } catch (RuntimeException e) {
                 nExcptn += 1;
                 System.out.println("method   " + methodTest.getName() + " called with exception");
             }
-            for(var methodAfter:methodsAfter){callMethod(obj, methodAfter.getName());}
+            for (var methodAfter : methodsAfter) {
+                var objForAfter = constr.newInstance();
+                callMethod(objForAfter, methodAfter.getName());
+            }
         }
-        System.out.println("\n\n total number of methods tested = "+(nOK+nExcptn)+"\n number of OK calls = " + nOK + "\n number of Exception calls = " + nExcptn);
+        System.out.println("\n\n total number of methods tested = " + (nOK + nExcptn) + "\n number of OK calls = " + nOK + "\n number of Exception calls = " + nExcptn);
     }
-
 
 
     public static Object callMethod(Object object, String name, Object... args) {
