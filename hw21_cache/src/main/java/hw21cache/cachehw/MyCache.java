4abb37304ec772c3d5477407c47aa956552ec8df
Zoufar <zuf_13@mail.ru>
2021-03-28 22:53:58 +0300

    hw21_amendments

--- hw21_cache/src/main/java/hw21cache/cachehw/MyCache.java
+++ hw21_cache/src/main/java/hw21cache/cachehw/MyCache.java
@@ -19,81 +19,72 @@ public class MyCache<K, V> implements HwCache<K, V> {
 
     @Override
     public void put(K key, V value) {
-          if ((key.getClass() == Integer.class) || (key.getClass() == Long.class)){
-          String keyString = key.toString();
-          datastoreIntLong.put(keyString,value);
+        if (checkKeyIfIntOrLong(key)){
+            datastoreIntLong.put(key.toString(),value);
         }
-        else { dataStore.put(key,value); }
-        for (var weakLsnr : weakListeners) {
-            HwListener<K,V> lsnr = weakLsnr.get();
-            if (lsnr != null ) lsnr.notify(key, value, "put");
+        else {
+            dataStore.put(key,value);
         }
+        notify(key, value, "put");
     }
 
     @Override
     public void remove(K key) {
         V value;
-        if ((key.getClass() == Integer.class) || (key.getClass() == Long.class)){
-            String keyString = key.toString();
-            value = datastoreIntLong.remove(keyString);
+        if (checkKeyIfIntOrLong(key)){
+            value = datastoreIntLong.remove(key.toString());
         }
-        else { value = dataStore.remove(key); }
-        for (var weakLsnr : weakListeners) {
-            HwListener<K,V> lsnr = weakLsnr.get();
-            if (lsnr != null ) lsnr.notify(key, value, "remove");
+        else {
+            value = dataStore.remove(key);
         }
+        notify(key, value, "remove");
     }
 
     @Override
     public V get(K key) {
         V value;
-        if ((key.getClass() == Integer.class) || (key.getClass() == Long.class)){
-            String keyString = key.toString();
-            value = datastoreIntLong.get(keyString);
+        if (checkKeyIfIntOrLong(key)){
+            value = datastoreIntLong.get(key.toString());
         }
-        else { value = dataStore.get(key); }
+        else {
+            value = dataStore.get(key);
+        }
         if (value != null) {
-            for (var weakLsnr : weakListeners) {
-                HwListener<K,V> lsnr = weakLsnr.get();
-                if (lsnr != null ) lsnr.notify(key, value, "get");
-            }
+            notify(key, value, "get");
+        }
+        else {
+            notify(key, value, "got null");
         }
         return value;
     }
 
     @Override
     public  boolean containsKey(K key) {
-        if ((key.getClass() == Integer.class) || (key.getClass() == Long.class)) {
-            String keyString = key.toString();
-            return datastoreIntLong.containsKey(keyString);
+        if (checkKeyIfIntOrLong(key)) {
+            return datastoreIntLong.containsKey(key.toString());
         }
-            return dataStore.containsKey(key);
+        return dataStore.containsKey(key);
     }
 
     @Override
     public void addListener(HwListener<K, V> listener) {
-        ArrayList<HwListener<K,V>> lsnrs = new ArrayList<>();
         for (var weakLsnr : weakListeners) {
-            lsnrs.add(weakLsnr.get());
+            if(listener == weakLsnr.get()) return;
         }
-        if(lsnrs.contains(listener)) return;
+
         WeakReference<HwListener<K,V>> weakLsnr = new WeakReference<>(listener);
         weakListeners.add(weakLsnr);
     }
 
     @Override
     public void removeListener(HwListener<K, V> listener) {
-        ArrayList<HwListener<K,V>> lsnrs = new ArrayList<>();
-            for (var weakLsnr : weakListeners) {
-                lsnrs.add(weakLsnr.get());
+        for (int i = weakListeners.size()-1; i >=0; i-- ) {
+            HwListener<K, V> lsnr = weakListeners.get(i).get();
+            if (lsnr == listener || lsnr == null) {
+                weakListeners.remove(i);
             }
-            lsnrs.remove(listener);
-            weakListeners.clear();
-        for (var lsnr : lsnrs) {
-            weakListeners.add(new WeakReference<>(lsnr));
         }
     }
-
 
     @Override
     public int size(){
@@ -103,6 +94,25 @@ public class MyCache<K, V> implements HwCache<K, V> {
     @Override
     public int sizeListenersList(){
         return weakListeners.size();
+    }
+
+    private boolean checkKeyIfIntOrLong(K key) {
+        return (key.getClass() == Integer.class) || (key.getClass() == Long.class);
+    }
+
+    private void notify(K key, V value, String action){
+        for (int i = weakListeners.size()-1; i >=0; i-- ) {
+            HwListener<K, V> lsnr = weakListeners.get(i).get();
+            if (lsnr != null) {
+                try {
+                    lsnr.notify(key, value, action);
+                } catch (RuntimeException e) {
+                    logger.error("listener exception");}
+            }
+            else {
+                weakListeners.remove(i);
+            }
+        }
     }
 
 }
